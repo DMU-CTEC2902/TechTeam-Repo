6a4efbdb6a97b21c82713b90515d7f5fe9651163
Dylan Patel <46681645+DylanPatel31@users.noreply.github.com>
2019-03-18 17:02:02 +0000

    Add files via upload
    models

---
+++ FilmMVC/FilmMVC/Models/Actor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FilmMVC.Models
+{
+    public class Actor
+    {
+        public virtual int ActorId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string DateOfBirth { get; set; }
+
+    }
+}
---
+++ FilmMVC/FilmMVC/Models/MovieActor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FilmMVC.Models
+{
+    public class MovieActor
+    {
+        public virtual int MovieActorId { get; set; }
+
+        public virtual int ActorId { get; set; }
+        public virtual int FilmId { get; set; }
+
+    }
+}
