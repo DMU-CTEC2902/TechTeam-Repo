dc9e14df208b21675692e6ef495eb864a6275a14
DylanPatel31 <p16199904@my365.dmu.ac.uk>
2019-03-27 17:50:04 +0000

    pics

---
+++ FilmMVC/FilmMVC/Controllers/DirectorsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FilmMVC.Models;
+
+namespace FilmMVC.Controllers
+{
+    public class DirectorsController : Controller
+    {
+        private FilmContext db = new FilmContext();
+
+
+        // GET: Directors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Director director = db.Directors.Find(id);
+            if (director == null)
+            {
+                return HttpNotFound();
+            }
+            return View(director);
+        }
+  
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
